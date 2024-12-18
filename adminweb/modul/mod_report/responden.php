b7b4258e45e9f7b843f71d6a9c62c61c89b8993d
Khairul122 <khairulhuda242@gmail.com>
2024-12-18 17:16:36 +0700

    all clear #1

--- adminweb/modul/mod_report/responden.php
+++ adminweb/modul/mod_report/responden.php
@@ -1,77 +1,80 @@
 <style>
 	.btn {
-  display: inline-block;
-  padding: 6px 12px;
-  font-size: 14px;
-  font-weight: normal;
-  line-height: 1.42857143;
-  text-align: center;
-  white-space: nowrap;
-  vertical-align: middle;
-  -ms-touch-action: manipulation;
-      touch-action: manipulation;
-  cursor: pointer;
-  -webkit-user-select: none;
-     -moz-user-select: none;
-      -ms-user-select: none;
-          user-select: none;
-  background-image: none;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  background-color: #5cb85c; 
-  padding: 5px 10px;
-  font-size: 12px;
-  line-height: 1.5;
-  border-radius: 3px;
-  margin-top:10px;
-  margin-bottom: 10px;
-  color: white;
-}
-@font-face {
-  font-family: 'Glyphicons Halflings';
+		display: inline-block;
+		padding: 6px 12px;
+		font-size: 14px;
+		font-weight: normal;
+		line-height: 1.42857143;
+		text-align: center;
+		white-space: nowrap;
+		vertical-align: middle;
+		-ms-touch-action: manipulation;
+		touch-action: manipulation;
+		cursor: pointer;
+		-webkit-user-select: none;
+		-moz-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+		background-image: none;
+		border: 1px solid transparent;
+		border-radius: 4px;
+		background-color: #5cb85c;
+		padding: 5px 10px;
+		font-size: 12px;
+		line-height: 1.5;
+		border-radius: 3px;
+		margin-top: 10px;
+		margin-bottom: 10px;
+		color: white;
+	}
 
-  src: url('../../../fonts/glyphicons-halflings-regular.eot');
-  src: url('../../../fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('../../../fonts/glyphicons-halflings-regular.woff2') format('woff2'), url('../../../fonts/glyphicons-halflings-regular.woff') format('woff'), url('../../../fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('../../../fonts/glyphicons-halflings-regular.svg#glyphicons_halflingsregular') format('svg');
-}
-.glyphicon {
-  position: relative;
-  top: 1px;
-  display: inline-block;
-  font-family: 'Glyphicons Halflings';
-  font-style: normal;
-  font-weight: normal;
-  line-height: 1;
+	@font-face {
+		font-family: 'Glyphicons Halflings';
 
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-.glyphicon-print:before {
-  content: "\e045";
-}
-.glyphicon-arrow-left:before {
-  content: "\e091";
-}
+		src: url('../../../fonts/glyphicons-halflings-regular.eot');
+		src: url('../../../fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('../../../fonts/glyphicons-halflings-regular.woff2') format('woff2'), url('../../../fonts/glyphicons-halflings-regular.woff') format('woff'), url('../../../fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('../../../fonts/glyphicons-halflings-regular.svg#glyphicons_halflingsregular') format('svg');
+	}
+
+	.glyphicon {
+		position: relative;
+		top: 1px;
+		display: inline-block;
+		font-family: 'Glyphicons Halflings';
+		font-style: normal;
+		font-weight: normal;
+		line-height: 1;
+
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.glyphicon-print:before {
+		content: "\e045";
+	}
+
+	.glyphicon-arrow-left:before {
+		content: "\e091";
+	}
 </style>
 <?php
-if($_GET['act']=='detail')
-{
-error_reporting(1);
-session_start();
+if ($_GET['act'] == 'detail') {
+	error_reporting(1);
+	session_start();
 
 
-include "../../../koneksi.php";
-include "../../../fungsi/fungsi_indotgl.php";
-include "../../../fungsi/fungsi_rubah_tanda.php";
+	include "../../../koneksi.php";
+	include "../../../fungsi/fungsi_indotgl.php";
+	include "../../../fungsi/fungsi_rubah_tanda.php";
 
-$hasil = mysql_query("SELECT * FROM tdescription");
-$date = date('Y-m-d');
-$time = date('H:i:s');
+	$hasil = mysql_query("SELECT * FROM tdescription");
+	$date = date('Y-m-d');
+	$time = date('H:i:s');
 
 
-$responden = mysql_fetch_array(mysql_query("SELECT * FROM tanswer, tcompany WHERE tanswer.companyId = '$_GET[id]' AND tcompany.companyId = tanswer.companyId"));
-$dateIndo = tgl_indo($responden['dateSurvey']);
+	$responden = mysql_fetch_array(mysql_query("SELECT * FROM tanswer, tcompany WHERE tanswer.companyId = '$_GET[id]' AND tcompany.companyId = tanswer.companyId"));
+	$dateIndo = tgl_indo($responden['dateSurvey']);
 
-echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
+	echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 		<tr >
 			<td colspan='8'  bgcolor=#337ab7 style='border: none ;color:white;'>
 			<a href='../../master.php?module=hasil&sub=laporan'>
@@ -89,7 +92,7 @@ echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 			<td >Alamat</td><td width='1'>:</td><td><b>$responden[companyAddress]</b></td>
 		</tr>
 		<tr>
-			<td >Telp / HP</td> <td>:</td><td> <b>$responden[companyPhoneHP]</b></td>
+			<td >Telp / HP</td> <td>:</td><td> <b>$responden[companyPhoneHp]</b></td>
 		</tr>
 		<tr>
 			<td width=150>Tanggal Isi Survey</td> <td width=1>:</td><td > <b>$dateIndo </b></td>
@@ -114,7 +117,7 @@ echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 				
 			";
 	$no = 1;
-	while ($data = mysql_fetch_array($hasil)){
+	while ($data = mysql_fetch_array($hasil)) {
 		$descriptionId = $data[descriptionId];
 		$sql = mysql_query("SELECT SUM(jawabanA) As TotalA,
 							SUM(jawabanB) As TotalB,
@@ -122,13 +125,13 @@ echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 							SUM(jawabanD) As TotalD,
 							SUM(jawabanE) As TotalE
 							FROM tanswer WHERE descriptionId = '$descriptionId' AND companyId = '$_GET[id]'");
-		
-		while($oke = mysql_fetch_array($sql)){
-      $a=rubah($oke[TotalA]);
-      $b=rubah($oke[TotalB]);
-      $c=rubah($oke[TotalC]);
-      $d=rubah($oke[TotalD]);
-      $e=rubah($oke[TotalE]);
+
+		while ($oke = mysql_fetch_array($sql)) {
+			$a = rubah($oke[TotalA]);
+			$b = rubah($oke[TotalB]);
+			$c = rubah($oke[TotalC]);
+			$d = rubah($oke[TotalD]);
+			$e = rubah($oke[TotalE]);
 			echo "<tr valign=top >
 					<td align='center'>$no</td>
 					<td align='center'>$data[groupId]</td>
@@ -140,19 +143,19 @@ echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 					<td align='center'>$e</td>
 				  </tr>
 
-			  ";	 
-			 
+			  ";
+
 			$no++;
 		}
-		}
-			$data_count = mysql_fetch_array(mysql_query("SELECT 
+	}
+	$data_count = mysql_fetch_array(mysql_query("SELECT 
 								SUM(jawabanA) As TotalA,
 								SUM(jawabanB) As TotalB,
 								SUM(jawabanC) As TotalC,
 								SUM(jawabanD) As TotalD,
 								SUM(jawabanE) As TotalE
 								FROM tanswer WHERE companyId = '$_GET[id]'"));
-		echo "	<tr align='center'>
+	echo "	<tr align='center'>
 			
 				<td bgcolor=#c6e1f2 align='center' colspan='3'><b>Total</b></td>
 				<td bgcolor=#c6e1f2><b>$data_count[TotalA]</b></td>
@@ -169,13 +172,10 @@ echo "<center><table border=0 cellpadding=10 cellspacing=3 bgcolor= #e6e6e6>
 }
 
 
-if($_GET['act']=='hapus')
-{
+if ($_GET['act'] == 'hapus') {
 	include "../../../koneksi.php";
 	mysql_query("DELETE FROM tcompany WHERE companyId='$_GET[id]'");
-	
+
 	header('location:../../master.php?module=hasil&sub=laporan');
-	
 }
 ?>
- 
--- database/survey.sql
+++ database/survey.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2017 at 12:52 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: 18 Des 2024 pada 11.15
+-- Versi Server: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tanswer`
+-- Struktur dari tabel `tanswer`
 --
 
 CREATE TABLE `tanswer` (
@@ -40,178 +40,24 @@ CREATE TABLE `tanswer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tanswer`
+-- Dumping data untuk tabel `tanswer`
 --
 
 INSERT INTO `tanswer` (`Id`, `descriptionId`, `groupId`, `companyId`, `jawaban`, `jawabanA`, `jawabanB`, `jawabanC`, `jawabanD`, `jawabanE`) VALUES
-(4, 23, 7, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(5, 24, 7, '20160502 050042', 'A', 1, 0, 0, 0, 0),
-(6, 25, 7, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(7, 29, 8, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(8, 30, 8, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(9, 31, 8, '20160502 050042', 'C', 0, 0, 1, 0, 0),
-(10, 26, 9, '20160502 050042', 'C', 0, 0, 1, 0, 0),
-(11, 27, 9, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(12, 28, 9, '20160502 050042', 'C', 0, 0, 1, 0, 0),
-(13, 32, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(14, 33, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(15, 34, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(16, 35, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(17, 36, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(18, 37, 10, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(19, 38, 11, '20160502 050042', 'C', 0, 0, 1, 0, 0),
-(20, 39, 11, '20160502 050042', 'B', 0, 1, 0, 0, 0),
-(21, 40, 11, '20160502 050042', 'A', 1, 0, 0, 0, 0),
-(25, 23, 7, '20160502 050129', 'A', 1, 0, 0, 0, 0),
-(26, 24, 7, '20160502 050129', 'A', 1, 0, 0, 0, 0),
-(27, 25, 7, '20160502 050129', 'A', 1, 0, 0, 0, 0),
-(28, 29, 8, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(29, 30, 8, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(30, 31, 8, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(31, 26, 9, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(32, 27, 9, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(33, 28, 9, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(34, 32, 10, '20160502 050129', 'A', 1, 0, 0, 0, 0),
-(35, 33, 10, '20160502 050129', 'A', 1, 0, 0, 0, 0),
-(36, 34, 10, '20160502 050129', 'C', 0, 0, 1, 0, 0),
-(37, 35, 10, '20160502 050129', 'B', 0, 1, 0, 0, 0),
-(38, 36, 10, '20160502 050129', 'C', 0, 0, 1, 0, 0),
-(39, 37, 10, '20160502 050129', 'C', 0, 0, 1, 0, 0),
-(40, 38, 11, '20160502 050129', 'D', 0, 0, 0, 1, 0),
-(41, 39, 11, '20160502 050129', 'C', 0, 0, 1, 0, 0),
-(42, 40, 11, '20160502 050129', 'E', 0, 0, 0, 0, 0),
-(46, 23, 7, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(47, 24, 7, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(48, 25, 7, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(49, 29, 8, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(50, 30, 8, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(51, 31, 8, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(52, 26, 9, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(53, 27, 9, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(54, 28, 9, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(55, 32, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(56, 33, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(57, 34, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(58, 35, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(59, 36, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(60, 37, 10, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(61, 38, 11, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(62, 39, 11, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(63, 40, 11, '20160502 050256', 'B', 0, 1, 0, 0, 0),
-(67, 23, 7, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(68, 24, 7, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(69, 25, 7, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(70, 29, 8, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(71, 30, 8, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(72, 31, 8, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(73, 26, 9, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(74, 27, 9, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(75, 28, 9, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(76, 32, 10, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(77, 33, 10, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(78, 34, 10, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(79, 35, 10, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(80, 36, 10, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(81, 37, 10, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(82, 38, 11, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(83, 39, 11, '20160502 050515', 'B', 0, 1, 0, 0, 0),
-(84, 40, 11, '20160502 050515', 'C', 0, 0, 1, 0, 0),
-(88, 23, 7, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(89, 24, 7, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(90, 25, 7, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(91, 29, 8, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(92, 30, 8, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(93, 31, 8, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(94, 26, 9, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(95, 27, 9, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(96, 28, 9, '20160502 050747', 'C', 0, 0, 1, 0, 0),
-(97, 32, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(98, 33, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(99, 34, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(100, 35, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(101, 36, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(102, 37, 10, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(103, 38, 11, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(104, 39, 11, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(105, 40, 11, '20160502 050747', 'B', 0, 1, 0, 0, 0),
-(109, 23, 7, '20160502 104906', 'A', 1, 0, 0, 0, 0),
-(110, 24, 7, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(111, 25, 7, '20160502 104906', 'D', 0, 0, 0, 1, 0),
-(112, 29, 8, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(113, 30, 8, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(114, 31, 8, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(115, 26, 9, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(116, 27, 9, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(117, 28, 9, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(118, 32, 10, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(119, 33, 10, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(120, 34, 10, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(121, 35, 10, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(122, 36, 10, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(123, 37, 10, '20160502 104906', 'A', 1, 0, 0, 0, 0),
-(124, 38, 11, '20160502 104906', 'C', 0, 0, 1, 0, 0),
-(125, 39, 11, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(126, 40, 11, '20160502 104906', 'B', 0, 1, 0, 0, 0),
-(130, 23, 7, '20160509 111626', 'E', 0, 0, 0, 0, 0),
-(131, 24, 7, '20160509 111626', 'E', 0, 0, 0, 0, 0),
-(132, 25, 7, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(133, 29, 8, '20160509 111626', 'D', 0, 0, 0, 1, 0),
-(134, 30, 8, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(135, 31, 8, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(136, 26, 9, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(137, 27, 9, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(138, 28, 9, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(139, 32, 10, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(140, 33, 10, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(141, 34, 10, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(142, 35, 10, '20160509 111626', 'D', 0, 0, 0, 1, 0),
-(143, 36, 10, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(144, 37, 10, '20160509 111626', 'D', 0, 0, 0, 1, 0),
-(145, 38, 11, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(146, 39, 11, '20160509 111626', 'C', 0, 0, 1, 0, 0),
-(147, 40, 11, '20160509 111626', 'B', 0, 1, 0, 0, 0),
-(148, 23, 7, '20160929 042525', 'E', 0, 0, 0, 0, 0),
-(149, 24, 7, '20160929 042525', 'B', 0, 1, 0, 0, 0),
-(150, 25, 7, '20160929 042525', 'E', 0, 0, 0, 0, 0),
-(151, 29, 8, '20160929 042525', 'E', 0, 0, 0, 0, 0),
-(152, 30, 8, '20160929 042525', 'D', 0, 0, 0, 1, 0),
-(153, 31, 8, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(154, 26, 9, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(155, 27, 9, '20160929 042525', 'D', 0, 0, 0, 1, 0),
-(156, 28, 9, '20160929 042525', 'B', 0, 1, 0, 0, 0),
-(157, 32, 10, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(158, 33, 10, '20160929 042525', 'D', 0, 0, 0, 1, 0),
-(159, 34, 10, '20160929 042525', 'D', 0, 0, 0, 1, 0),
-(160, 35, 10, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(161, 36, 10, '20160929 042525', 'D', 0, 0, 0, 1, 0),
-(162, 37, 10, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(163, 38, 11, '20160929 042525', 'C', 0, 0, 1, 0, 0),
-(164, 39, 11, '20160929 042525', 'B', 0, 1, 0, 0, 0),
-(165, 40, 11, '20160929 042525', 'B', 0, 1, 0, 0, 0),
-(166, 23, 7, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(167, 24, 7, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(168, 25, 7, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(169, 29, 8, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(170, 30, 8, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(171, 31, 8, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(172, 26, 9, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(173, 27, 9, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(174, 28, 9, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(175, 41, 9, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(176, 32, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(177, 33, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(178, 34, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(179, 35, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(180, 36, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(181, 37, 10, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(182, 38, 11, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(183, 39, 11, '20161126 065414', 'B', 0, 1, 0, 0, 0),
-(184, 40, 11, '20161126 065414', 'B', 0, 1, 0, 0, 0);
+(204, 46, 15, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(205, 47, 15, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(206, 48, 15, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(207, 49, 16, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(208, 50, 16, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(209, 51, 16, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(210, 52, 17, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(211, 53, 17, '20241218 110846', 'A', 1, 0, 0, 0, 0),
+(212, 54, 17, '20241218 110846', 'A', 1, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tcompany`
+-- Struktur dari tabel `tcompany`
 --
 
 CREATE TABLE `tcompany` (
@@ -220,34 +66,20 @@ CREATE TABLE `tcompany` (
   `companyAddress` text NOT NULL,
   `companyPhoneHp` varchar(30) NOT NULL,
   `dateSurvey` varchar(30) NOT NULL,
-  `suggestion` text NOT NULL,
-  `product` varchar(20) NOT NULL
+  `suggestion` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tcompany`
+-- Dumping data untuk tabel `tcompany`
 --
 
-INSERT INTO `tcompany` (`companyId`, `companyName`, `companyAddress`, `companyPhoneHp`, `dateSurvey`, `suggestion`, `product`) VALUES
-('20160421 090324', 'ghf', 'ghf', ' / 0000', '2016-04-21', 'fhgd', 'Kartu AS'),
-('20160421 090459', 'hmhgjk', 'hjhgj', ' / hgjgf', '2016-04-21', 'hjgyjg', 'Kartu Halo'),
-('20160421 090708', 'jgj', 'jkhj', ' / 98', '2016-04-21', 'hjugj', 'Kartu Halo'),
-('20160421 090739', 'hjvj', 'khjkhj', ' / 8787', '2016-04-21', 'jkh', 'Kartu Halo'),
-('20160501 023324', 'aria', 'aaa', ' / 00000000000000', '2016-05-01', 'aaa', 'Kartu AS'),
-('20160502 050042', 'ANdi', 'jambi', ' / 0811111', '2016-05-02', 'Bangus', 'Kartu Halo'),
-('20160502 050129', 'Andre Oktari', 'Jelutung,Kota jambi', ' / 081364631947', '2016-05-02', 'Saya ingin memberikan saran pada penetapan harga untuk tarif internet trlalu mahal', 'Kartu Simpati'),
-('20160502 050256', 'adir', 'jl.indah', ' / 082145678909', '2016-05-02', 'okey', 'Kartu Simpati'),
-('20160502 050515', 'afri', 'jl.bromo', ' / 082312345678', '2016-05-02', 'good', 'Kartu AS'),
-('20160502 050747', 'boi', 'jl.cinta', ' / 082145678900', '2016-05-02', 'awesome', 'Kartu Simpati'),
-('20160502 104906', 'adi ariawan', 'kerinci', ' / 082344232332', '2016-05-02', 'Oke bang', 'Kartu AS'),
-('20160509 111626', 'wahyu', 'jambi', ' / 00000000000000', '2016-05-09', 'tolong ditingkatkan pelayanannya', 'Kartu AS'),
-('20160929 042525', 'andi saputra', 'jambi', ' / 082222222222', '2016-09-29', 'jos gandos', 'Kartu Simpati'),
-('20161126 065414', 'badu', 'jambi', ' / 0088888', '2016-11-26', 'sudah baik', 'Kartu Simpati');
+INSERT INTO `tcompany` (`companyId`, `companyName`, `companyAddress`, `companyPhoneHp`, `dateSurvey`, `suggestion`) VALUES
+('20241218 110846', 'Hanafi', 'Tarandam', '082165443678', '2024-12-18', 'Udah bagus');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tdescription`
+-- Struktur dari tabel `tdescription`
 --
 
 CREATE TABLE `tdescription` (
@@ -261,34 +93,24 @@ CREATE TABLE `tdescription` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tdescription`
+-- Dumping data untuk tabel `tdescription`
 --
 
 INSERT INTO `tdescription` (`descriptionId`, `description`, `groupId`, `CreatedDate`, `CreatedUser`, `ModifiedDate`, `ModifiedUser`) VALUES
-(23, 'Keramahan Customer Service', 7, '2016-05-02 03:15:43', 1, '0000-00-00 00:00:00', 0),
-(24, 'Kecepatan Respon Customer Service', 7, '2016-05-02 03:16:02', 1, '0000-00-00 00:00:00', 0),
-(25, 'Solusi yang Diberikan', 7, '2016-05-02 03:16:25', 1, '0000-00-00 00:00:00', 0),
-(26, 'Kualitas jaringan pada jam sibuk', 9, '2016-05-02 03:17:27', 1, '0000-00-00 00:00:00', 0),
-(27, 'Kualitas Jaringan telefon  pada saat cuaca buruk', 9, '2016-05-02 03:17:48', 1, '0000-00-00 00:00:00', 0),
-(28, 'Kecepatan Penanganan Jika ada masalah pada jaringan telefon', 9, '2016-05-02 03:18:44', 1, '0000-00-00 00:00:00', 0),
-(29, 'Kecapatan SMS dikirim dan diterima', 8, '2016-05-02 03:20:12', 1, '0000-00-00 00:00:00', 0),
-(30, 'Kualitas jaringan pengiriman SMS pada jam sibuk', 8, '2016-05-02 03:20:50', 1, '0000-00-00 00:00:00', 0),
-(31, 'Kualitas pengiriman SMS pada cuaca buruk', 8, '2016-05-02 03:21:20', 1, '0000-00-00 00:00:00', 0),
-(32, 'Kecepatan Akses internet', 10, '2016-05-02 03:21:44', 1, '0000-00-00 00:00:00', 0),
-(33, 'Kualitas jaringan', 10, '2016-05-02 03:22:00', 1, '0000-00-00 00:00:00', 0),
-(34, 'Luas area cakupan jaringan internet', 10, '2016-05-02 03:22:31', 1, '0000-00-00 00:00:00', 0),
-(35, 'Kualitas jaringan untuk koneksi realtime', 10, '2016-05-02 03:23:18', 1, '0000-00-00 00:00:00', 0),
-(36, 'Kualitas jaringan pada jam sibuk', 10, '2016-05-02 03:23:41', 1, '0000-00-00 00:00:00', 0),
-(37, 'Kulitas jaringan internet pada cuaca buruk', 10, '2016-05-02 03:24:02', 1, '0000-00-00 00:00:00', 0),
-(38, 'penetapan harga untuk tarif telefon', 11, '2016-05-02 03:24:55', 1, '0000-00-00 00:00:00', 0),
-(39, 'penetapan harga untuk tarif SMS', 11, '2016-05-02 03:25:04', 1, '0000-00-00 00:00:00', 0),
-(40, 'penetapan harga untuk tarif internet', 11, '2016-05-02 03:25:20', 1, '0000-00-00 00:00:00', 0),
-(41, '							sdsdsssss				', 9, '2016-09-29 16:28:53', 1, '2016-09-29 16:29:03', 1);
+(46, 'Seberapa puas Anda dengan layanan yang diberikan oleh apotek kami?', 15, '2024-12-18 11:05:56', 1, '0000-00-00 00:00:00', 0),
+(47, 'Apakah staf kami ramah dan membantu saat melayani Anda?', 15, '2024-12-18 11:06:03', 1, '0000-00-00 00:00:00', 0),
+(48, 'Apakah waktu tunggu untuk mendapatkan obat sesuai dengan harapan Anda?', 15, '2024-12-18 11:06:09', 1, '0000-00-00 00:00:00', 0),
+(49, 'Apakah lokasi apotek kami mudah diakses dari tempat tinggal Anda?', 16, '2024-12-18 11:06:24', 1, '0000-00-00 00:00:00', 0),
+(50, 'Apakah Anda merasa tata letak di dalam apotek memudahkan pencarian produk?', 16, '2024-12-18 11:06:33', 1, '0000-00-00 00:00:00', 0),
+(51, 'Apakah jam operasional apotek sudah sesuai dengan kebutuhan Anda?\r\n', 16, '2024-12-18 11:06:44', 1, '0000-00-00 00:00:00', 0),
+(52, 'Apakah Anda merasa informasi yang diberikan apoteker tentang penggunaan obat jelas dan mudah dipahami?', 17, '2024-12-18 11:07:01', 1, '0000-00-00 00:00:00', 0),
+(53, 'Apakah apotek kami menyediakan semua obat atau produk kesehatan yang Anda butuhkan?', 17, '2024-12-18 11:07:10', 1, '0000-00-00 00:00:00', 0),
+(54, 'Apakah harga produk di apotek kami sesuai dengan kualitas dan pelayanan yang diberikan?\r\n', 17, '2024-12-18 11:07:18', 1, '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tgroup`
+-- Struktur dari tabel `tgroup`
 --
 
 CREATE TABLE `tgroup` (
@@ -301,20 +123,18 @@ CREATE TABLE `tgroup` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tgroup`
+-- Dumping data untuk tabel `tgroup`
 --
 
 INSERT INTO `tgroup` (`groupId`, `groupName`, `CreatedDate`, `CreatedUser`, `ModifiedDate`, `ModifiedUser`) VALUES
-(7, 'Pelayanan Konsumen', '2016-05-02 03:11:42', 1, '0000-00-00 00:00:00', 0),
-(8, 'Kualitas SMS', '2016-05-02 03:12:03', 1, '0000-00-00 00:00:00', 0),
-(9, 'Kulitas Jaringan Telefon', '2016-05-02 03:12:26', 1, '0000-00-00 00:00:00', 0),
-(10, 'Kualitas Akses Layanan Internet', '2016-05-02 03:12:38', 1, '0000-00-00 00:00:00', 0),
-(11, 'Tarif untuk setiap layanan', '2016-05-02 03:19:35', 1, '2016-05-02 04:13:11', 1);
+(15, 'Kepuasan Pelanggan', '2024-12-18 11:05:45', 1, '0000-00-00 00:00:00', 0),
+(16, 'Kemudahan dan Kenyamanan', '2024-12-18 11:06:16', 1, '0000-00-00 00:00:00', 0),
+(17, 'Kualitas Pelayanan', '2024-12-18 11:06:51', 1, '0000-00-00 00:00:00', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tuser`
+-- Struktur dari tabel `tuser`
 --
 
 CREATE TABLE `tuser` (
@@ -327,11 +147,12 @@ CREATE TABLE `tuser` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tuser`
+-- Dumping data untuk tabel `tuser`
 --
 
 INSERT INTO `tuser` (`userId`, `username`, `password`, `fullname`, `email`, `level`) VALUES
-(1, 'wahyu', '32c9e71e866ecdbc93e497482aa6779f', 'wahyu budiman', 'wahyu@gmail.com', 'Super');
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'admin@gmail.com', 'Super'),
+(5, 'tes', '28b662d883b6d76fd96e4ddc5e9ba780', 'Tes', 'tes@gmail.com', 'Biasa');
 
 --
 -- Indexes for dumped tables
@@ -384,28 +205,28 @@ ALTER TABLE `tuser`
 -- AUTO_INCREMENT for table `tanswer`
 --
 ALTER TABLE `tanswer`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=185;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=213;
 --
 -- AUTO_INCREMENT for table `tdescription`
 --
 ALTER TABLE `tdescription`
-  MODIFY `descriptionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `descriptionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 --
 -- AUTO_INCREMENT for table `tgroup`
 --
 ALTER TABLE `tgroup`
-  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `groupId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `tuser`
 --
 ALTER TABLE `tuser`
-  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tanswer`
+-- Ketidakleluasaan untuk tabel `tanswer`
 --
 ALTER TABLE `tanswer`
   ADD CONSTRAINT `tanswer_ibfk_1` FOREIGN KEY (`companyId`) REFERENCES `tcompany` (`companyId`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -413,7 +234,7 @@ ALTER TABLE `tanswer`
   ADD CONSTRAINT `tanswer_ibfk_4` FOREIGN KEY (`descriptionId`) REFERENCES `tdescription` (`descriptionId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `tdescription`
+-- Ketidakleluasaan untuk tabel `tdescription`
 --
 ALTER TABLE `tdescription`
   ADD CONSTRAINT `tdescription_ibfk_1` FOREIGN KEY (`groupId`) REFERENCES `tgroup` (`groupId`) ON DELETE CASCADE ON UPDATE CASCADE;
